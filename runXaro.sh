87b002ad6ce526065c3bd67c024468f75772cc51
Damien Stamates <thedoubledimensions@gmail.com>
2018-08-23 15:55:21 -0500

    Updated bash scripts

--- runXaro.sh
+++ runXaro.sh
@@ -1,1 +1,28 @@
+build () {
+    buildMsg="Building Xaro"
+    
+    if [[ $2 == -verbose || $2 == -v  || $1 == -verbose || $1 == -v ]]; then
+        buildMsg="$buildMsg Verbosely..."
+        echo $buildMsg
+
+        mage -v
+    else
+        buildMsg="$buildMsg Quietly..."
+        echo $buildMsg
+        
+        mage
+    fi
+}
+
+if [[ $1 == -build || $1 == -b ]]; then
+    build $1 $2
+fi
+
+if [ ! -f ./bin/Xaro.exe ]; then
+    echo "./bin/Xaro.exe was not found!"
+    build $1 $2
+fi
+
+echo "Running Xaro..."
+
 ./bin/Xaro.exe
