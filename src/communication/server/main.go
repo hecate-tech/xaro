ec383b20bdb6a5bfa81717c398c4c5a6dbc2caab
Damien Stamates <thedoubledimensions@gmail.com>
2018-08-30 02:04:26 -0500

    Re added server's main.go file

---
+++ src/communication/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/damienfamed75/engo-xaro/src/communication"
+	pb "github.com/damienfamed75/engo-xaro/src/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":8081"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	fmt.Println("Server Created! Listening for clients...")
+	s := grpc.NewServer()
+	pb.RegisterXaroServer(s, communication.NewServer())
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
